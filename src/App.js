71895d05d62c67602effffef23ea608a78127b23
Your Name <you@example.com>
2019-10-02 08:18:07 -0500

    need to set up rendering the data

--- src/App.js
+++ src/App.js
@@ -9,16 +9,53 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-        groceries: Data
+        tasks: Data
     };
   }
+
+  toggleItem = (event, itemId ) => {
+    event.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.map(item => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+      }
+    })
+    });
+  };
+
+  clearTasks = event => {
+    event.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.filter(item => {
+        return !item.completed;
+      })
+    });
+  };
+
+  addItem = (event, itemName) => {
+    const newItem = {
+      id: Date.now(),
+      name: itemName,
+      completed: false
+    };
+
+    this.state({
+      tasks: [...this.state.tasks, newItem]
+    });
+  };
 
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="App">
         <h2>Welcome to your Todo App!</h2>
       </div>
     );
