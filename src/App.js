614751bdb69bc34ce077e6281e4cef9fbda633ab
Your Name <you@example.com>
2019-10-02 08:07:46 -0500

    have components set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import Data from "./components/TodoComponents/TodoList";
+import ToDo from "./components/TodoComponents/Todo";
+import ToDoForm from "./components/TodoComponents/TodoForm";
+
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        groceries: Data
+    };
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
